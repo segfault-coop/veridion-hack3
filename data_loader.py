a83ced6159d0fe1ad4a360124d4826a5fdcf1dba
Radu M <radu.moldoveanuf@gmail.com>
2025-03-29 11:38:05 +0200

    Added action_cost

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,6 @@ def action_costs(json_file_path: str = "data/cost.json") -> dict:
     with open(json_file_path, 'r') as file:
         data = json.load(file)
     
-    # Create a dictionary mapping names to costs
     cost = {}
     for item_data in data.values():
         cost[item_data["text"]] = item_data["cost"]
