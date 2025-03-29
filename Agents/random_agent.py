0967df7699f25405914d57c2dbf89c8530c15a8f
Sebastian-Ion Nae <naesebi2000@gmail.com>
2025-03-29 11:52:57 +0200

    Added Agent history (#2)

--- Agents/random_agent.py
+++ Agents/random_agent.py
@@ -1,7 +1,17 @@
 class RandomAgent:
     def __init__(self, player_id):
         self.player_id = player_id
-        self.actions_costs = []``
+        self.actions_costs = []
+        self.history = {
+            "word_given": [],
+            "word_used": [],
+            "cost": []
+        }
         
+    def history(self, state, word_given, word_used, cost):
+        self.history["word_given"].append(word_given)
+        self.history["word_used"].append(word_used)
+        self.history["cost"].append(cost)
+    
     def action(self, state):
         return random.choice(self.actions_costs)
