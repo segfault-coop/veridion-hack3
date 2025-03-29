ea20db0c38953f5ad46a9e7f4868c399fc06fd7a
SheepSeb <naesebi2000@gmail.com>
2025-03-29 11:48:45 +0200

    Added Agent history

--- Agents/single_llm.py
+++ Agents/single_llm.py
@@ -1,10 +1,15 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
+from pydantic import BaseModel
+
+class WordUsed(BaseModel):
+    word: str = Field(description="The word used by the player")
 
 class SingleLLM:
     def __init__(self, player_id):
         self.player_id = player_id
         self.llm = OllamaLLM(model="llama3.1:8b")
+        self.actions_costs = []
         self.prompt = ChatPromptTemplate.from_template(
             "You are a helpful assistant. You are given a question and you need to answer it."
         )
